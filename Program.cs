e9d8839221e9245fd89f6c520025d6aa190b0090
Abdoulaye Pangnon KOULIBALY <abdoulaye-pangnon.koulibaly@talan.com>
2023-11-21 00:13:35 +0100

    vv

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace programme_fichiers
 {
@@ -6,7 +8,45 @@ namespace programme_fichiers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string filename = "nomFichier.txt";
+            //File.WriteAllText("nomFichier.txt", "Voici le contenue que j'ai écrit dans le fichier texte ");
+            //File.AppendAllText(filename, " Je rajoute ce texte");
+
+            var noms = new List<string>
+            {
+                "Jean",
+                "Paul",
+                "Martin"
+            };
+
+            File.WriteAllLines(filename, noms);
+
+
+            try
+            {
+                //int a = 1;
+                //a = a / 0;
+                //     string resultat = File.ReadAllText("nomFichier.txt");
+
+                var lignes = File.ReadAllLines("nomFichier.txt");
+
+                foreach (var ligne in lignes) {
+
+                    Console.WriteLine(ligne);
+                }
+
+                
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("ERREUR:Ce fichier n'existe pas (" + ex.Message + ")");
+            }
+
+            catch
+            {
+                Console.WriteLine("UNE ERREUR inconnu est arrivé");
+            }
+
         }
     }
 }
